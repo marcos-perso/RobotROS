8f18066d50810e3d03a3c8be778d5b551852e615
Marcos <marcos.martinez@gmail.com>
2018-11-10 09:41:15 +0100

    Some cleaning

--- catkin_robotROS/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/robotROS/catkin_robotROS/src
-# Build directory: /home/robot/robotROS/catkin_robotROS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
